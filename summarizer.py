7f233186d2254ff9166f3f6df3ddddd80646576e
nwjbrandon <nwjbrandon@gmail.com>
2025-04-01 00:10:29 +0800

    update

--- summarizer.py
+++ summarizer.py
@@ -1,3 +1,5 @@
+import glob
+
 import pandas as pd
 
 from rouge_score import rouge_scorer
@@ -58,8 +60,25 @@ class TextSummarizer:
 
 class BBCNews_Dataset:
 
-    def __init__(self):
-        pass
+    def __init__(self, filenames):
+        self.filenames = glob.glob(filenames)
+
+    def __len__(self):
+        return len(self.filenames)
+    
+    def get(self, i):
+        article_filename = self.filenames[i]
+        with open(article_filename) as f:
+            article = f.read()
+
+        summary_filename = article_filename.replace("articles", "summaries")
+        with open(summary_filename) as f:
+            summary = f.read()
+
+        return {
+            "article": article,
+            "summary": summary
+        }
     
 class CNNNews_Dataset:
 
@@ -78,30 +97,20 @@ class CNNNews_Dataset:
 
 
 if __name__ == "__main__":
-    cnn_news_dataset = CNNNews_Dataset(
-        filenames=[
-            # pd.read_csv("datasets/text_summary/cnn_news/train.csv"),
-            pd.read_csv("datasets/text_summary/cnn_news/test.csv"),
-            # pd.read_csv("datasets/text_summary/cnn_news/validation.csv"),
-        ]
-    )
-    print(len(cnn_news_dataset))
-    data = cnn_news_dataset.get(0)
-    # print(cnn_news_dataset.get(0))
+    # filenames = [
+    #     pd.read_csv("datasets/text_summary/cnn_news/train.csv"),
+    #     pd.read_csv("datasets/text_summary/cnn_news/test.csv"),
+    #     pd.read_csv("datasets/text_summary/cnn_news/validation.csv"),
+    # ]
+    # dataset = CNNNews_Dataset(filenames=filenames)
+
+    filenames = "datasets/text_summary/bbc_news/articles/*/*.txt"
+    dataset = BBCNews_Dataset(filenames=filenames)
+
+    data = dataset.get(0)
     text = data["article"]
     reference_summary = data["summary"]
     print(text)
-    # raise
-    # text = """
-    # Artificial Intelligence (AI) is the simulation of human intelligence in machines that are programmed to think and learn.
-    # AI is a broad field of study that includes machine learning, natural language processing, robotics, and more.
-    # It is widely used in various industries such as healthcare, finance, and autonomous systems.
-    # AI-powered systems can process vast amounts of data and make predictions, enhancing efficiency and decision-making.
-    # """
-    
-    # reference_summary = """
-    # AI simulates human intelligence in machines and is applied in fields like healthcare and finance to improve efficiency.
-    # """
 
     text_summarizer = TextSummarizer()
     methods = ["luhn", "lsa", "lex_rank", "text_rank"]
