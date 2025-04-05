38b1c2c5999fbbb43ec63092734213afe5b3f143
nwjbrandon <nwjbrandon@gmail.com>
2025-04-05 21:23:36 +0800

    refactor: add evaluation code for models

--- text_summarizer.py
+++ text_summarizer.py
@@ -1,7 +1,10 @@
+import nltk
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
 from transformers import pipeline
-
 from rouge_score import rouge_scorer
-
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.summarizers.lsa import LsaSummarizer                 
@@ -9,17 +12,26 @@ from sumy.summarizers.luhn import LuhnSummarizer
 from sumy.summarizers.lex_rank import LexRankSummarizer  
 from sumy.summarizers.text_rank import TextRankSummarizer
 
+
+nltk.download('punkt_tab')
+
 class TextSummarizer:
 
     def __init__(self):
         self.summarizers = {
-            "luhn": self.luhn_summarizer,
-            "lsa": self.lsa_summarizer,
-            "lex_rank": self.lex_rank_summarizer,
-            "text_rank": self.text_rank_summarizer,
-            "bart": self.facebook_bart_summarizer,
+            "Luhn": self.luhn_summarizer,
+            "LSA": self.lsa_summarizer,
+            "LexRank": self.lex_rank_summarizer,
+            "TextRank": self.text_rank_summarizer,
+            "BART": self.facebook_bart_summarizer,
         }
         self.algorithms = [summarizer for summarizer in self.summarizers]
+
+        self.summarizer_luhn = LuhnSummarizer()   
+        self.summarizer_lsa = LsaSummarizer()   
+        self.summarizer_lexrank = LexRankSummarizer()   
+        self.summarizer_textrank = TextRankSummarizer()
+        self.summarizer_bart = pipeline("summarization", model="facebook/bart-large-cnn")
 
     def summarize_text(self, text, method):
         summarizer = self.summarizers[method]
@@ -28,36 +40,76 @@ class TextSummarizer:
 
     def luhn_summarizer(self, text, n_sentences=5):
         parser = PlaintextParser.from_string(text, Tokenizer("english"))
-        summarizer_lex = LuhnSummarizer()                      
-        summary = summarizer_lex(parser.document, n_sentences)              
+        summary = self.summarizer_luhn(parser.document, n_sentences)              
         return " ".join(str(sentence) for sentence in summary)
 
     def lsa_summarizer(self, text, n_sentences=5):
         parser = PlaintextParser.from_string(text, Tokenizer("english"))
-        summarizer_lex = LsaSummarizer()                      
-        summary = summarizer_lex(parser.document, n_sentences)              
+        summary = self.summarizer_lsa(parser.document, n_sentences)              
         return " ".join(str(sentence) for sentence in summary)
 
 
     def lex_rank_summarizer(self, text, n_sentences=5):
         parser = PlaintextParser.from_string(text, Tokenizer("english"))
         summarizer_lex = LexRankSummarizer()                      
-        summary = summarizer_lex(parser.document, n_sentences)              
+        summary = self.summarizer_lexrank(parser.document, n_sentences)              
         return " ".join(str(sentence) for sentence in summary)
 
 
-    def text_rank_summarizer(sellf, text, n_sentences=5):
+    def text_rank_summarizer(self, text, n_sentences=5):
         parser = PlaintextParser.from_string(text, Tokenizer("english"))
-        summarizer = TextRankSummarizer()
-        summary = summarizer(parser.document, n_sentences)
+        summary = self.summarizer_textrank(parser.document, n_sentences)
         return " ".join(str(sentence) for sentence in summary)
     
     def facebook_bart_summarizer(self, text, min_length=50, max_length=100):
-        summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-        return summarizer(text, min_length=min_length, max_length=max_length, do_sample=False)[0]["summary_text"]
+        return self.summarizer_bart(text, min_length=min_length, max_length=max_length, do_sample=False)[0]["summary_text"]
 
 
-def evaluate_rouge_score(reference_summary, generated_summary):
+def rouge_score_metric(reference_summary, generated_summary):
     scorer = rouge_scorer.RougeScorer(["rouge1", "rouge2", "rougeL"], use_stemmer=True)
     scores = scorer.score(reference_summary, generated_summary)
     return scores
+
+def evaluate_text_summarizer(text_summarizer, dataset, text_key, summary_key):
+    full_metrics = []
+
+    for algorithm in text_summarizer.algorithms:
+        rouge1_precisions = []
+        rouge1_recalls = []
+        rouge1_fmeasures = []
+
+        rouge2_precisions = []
+        rouge2_recalls = []
+        rouge2_fmeasures = []
+
+        rougeL_precisions = []
+        rougeL_recalls = []
+        rougeL_fmeasures = []
+
+        for data in tqdm(dataset):
+            text = data[text_key]
+            reference_summary = data[summary_key]
+            generated_summary = text_summarizer.summarize_text(text, algorithm)
+            rouge_scores = rouge_score_metric(reference_summary, generated_summary)
+            
+            for metric, score in rouge_scores.items():
+                if metric == "rouge1":
+                    rouge1_precisions.append(score.precision)
+                    rouge1_recalls.append(score.recall)
+                    rouge1_fmeasures.append(score.fmeasure)
+                if metric == "rouge2":
+                    rouge2_precisions.append(score.precision)
+                    rouge2_recalls.append(score.recall)
+                    rouge2_fmeasures.append(score.fmeasure)
+                if metric == "rougeL":
+                    rougeL_precisions.append(score.precision)
+                    rougeL_recalls.append(score.recall)
+                    rougeL_fmeasures.append(score.fmeasure)
+            break
+
+        full_metrics.append([algorithm, "Rouge1", np.mean(score.precision), np.mean(score.recall), np.mean(score.fmeasure)])
+        full_metrics.append([algorithm, "Rouge2", np.mean(score.precision), np.mean(score.recall), np.mean(score.fmeasure)])
+        full_metrics.append([algorithm, "RougeL", np.mean(score.precision), np.mean(score.recall), np.mean(score.fmeasure)])
+
+    df = pd.DataFrame(full_metrics, columns=["Algorithm", "Metric", "P", "R", "F1"])
+    return df
