f71ca0f2df049cd8349ce9294877fef4a5348e4a
nwjbrandon <nwjbrandon@gmail.com>
2025-04-05 19:18:39 +0800

    feat: add additional dataset

--- text_summarizer.py
+++ text_summarizer.py
@@ -1,9 +1,9 @@
+from transformers import pipeline
 
 from rouge_score import rouge_scorer
 
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.parsers.plaintext import PlaintextParser
-
 from sumy.summarizers.lsa import LsaSummarizer                 
 from sumy.summarizers.luhn import LuhnSummarizer             
 from sumy.summarizers.lex_rank import LexRankSummarizer  
@@ -17,43 +17,47 @@ class TextSummarizer:
             "lsa": self.lsa_summarizer,
             "lex_rank": self.lex_rank_summarizer,
             "text_rank": self.text_rank_summarizer,
+            "bart": self.facebook_bart_summarizer,
         }
         self.algorithms = [summarizer for summarizer in self.summarizers]
 
-    def summarize_text(self, text, method, n_sentences=3):
+    def summarize_text(self, text, method):
         summarizer = self.summarizers[method]
-        summary = summarizer(text, n_sentences=n_sentences)
+        summary = summarizer(text)
         return summary
 
-    def luhn_summarizer(self, text, n_sentences):
+    def luhn_summarizer(self, text, n_sentences=5):
         parser = PlaintextParser.from_string(text, Tokenizer("english"))
         summarizer_lex = LuhnSummarizer()                      
         summary = summarizer_lex(parser.document, n_sentences)              
         return " ".join(str(sentence) for sentence in summary)
 
-    def lsa_summarizer(self, text, n_sentences):
+    def lsa_summarizer(self, text, n_sentences=5):
         parser = PlaintextParser.from_string(text, Tokenizer("english"))
         summarizer_lex = LsaSummarizer()                      
         summary = summarizer_lex(parser.document, n_sentences)              
         return " ".join(str(sentence) for sentence in summary)
 
 
-    def lex_rank_summarizer(self, text, n_sentences):
+    def lex_rank_summarizer(self, text, n_sentences=5):
         parser = PlaintextParser.from_string(text, Tokenizer("english"))
         summarizer_lex = LexRankSummarizer()                      
         summary = summarizer_lex(parser.document, n_sentences)              
         return " ".join(str(sentence) for sentence in summary)
 
 
-    def text_rank_summarizer(sellf, text, n_sentences):
+    def text_rank_summarizer(sellf, text, n_sentences=5):
         parser = PlaintextParser.from_string(text, Tokenizer("english"))
         summarizer = TextRankSummarizer()
         summary = summarizer(parser.document, n_sentences)
         return " ".join(str(sentence) for sentence in summary)
+    
+    def facebook_bart_summarizer(self, text, min_length=50, max_length=100):
+        summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+        return summarizer(text, min_length=min_length, max_length=max_length, do_sample=False)[0]["summary_text"]
 
 
 def evaluate_rouge_score(reference_summary, generated_summary):
     scorer = rouge_scorer.RougeScorer(["rouge1", "rouge2", "rougeL"], use_stemmer=True)
     scores = scorer.score(reference_summary, generated_summary)
     return scores
-
