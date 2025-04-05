8ffa3f5e7cd39cf53cef1aa09ab320654613d3c9
nwjbrandon <nwjbrandon@gmail.com>
2025-04-05 19:10:33 +0800

    update

--- text_summarizer.py
+++ text_summarizer.py
@@ -1,4 +1,3 @@
-from datasets import load_dataset
 
 from rouge_score import rouge_scorer
 
@@ -19,6 +18,7 @@ class TextSummarizer:
             "lex_rank": self.lex_rank_summarizer,
             "text_rank": self.text_rank_summarizer,
         }
+        self.algorithms = [summarizer for summarizer in self.summarizers]
 
     def summarize_text(self, text, method, n_sentences=3):
         summarizer = self.summarizers[method]
@@ -51,28 +51,9 @@ class TextSummarizer:
         summary = summarizer(parser.document, n_sentences)
         return " ".join(str(sentence) for sentence in summary)
 
-    def evaluate_summary(self, reference_summary, generated_summary):
-        scorer = rouge_scorer.RougeScorer(["rouge1", "rouge2", "rougeL"], use_stemmer=True)
-        scores = scorer.score(reference_summary, generated_summary)
-        return scores
 
+def evaluate_rouge_score(reference_summary, generated_summary):
+    scorer = rouge_scorer.RougeScorer(["rouge1", "rouge2", "rougeL"], use_stemmer=True)
+    scores = scorer.score(reference_summary, generated_summary)
+    return scores
 
-if __name__ == "__main__":
-    ds = load_dataset("abisee/cnn_dailymail", "3.0.0")
-    data = ds["train"][0]
-
-    article = data["article"]
-    summary_ref = data["highlights"]
-    print(article)
-
-    text_summarizer = TextSummarizer()
-    methods = ["luhn", "lsa", "lex_rank", "text_rank"]
-    for method in methods:
-        summary_gen = text_summarizer.summarize_text(article, method, n_sentences=3)
-        print("Generated Summary:", summary_gen)
-    
-        scores = text_summarizer.evaluate_summary(summary_ref, summary_gen)
-
-        print("ROUGE Scores:")
-        for metric, score in scores.items():
-            print(f"{metric}: Precision={score.precision:.4f}, Recall={score.recall:.4f}, F1-score={score.fmeasure:.4f}")
