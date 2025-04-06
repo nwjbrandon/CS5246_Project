bcdf62d57694928a8c5fd9d8a5f041de8d73baee
nwjbrandon <nwjbrandon@gmail.com>
2025-04-06 10:51:30 +0800

    change dataset

--- text_summarizer.py
+++ text_summarizer.py
@@ -39,33 +39,32 @@ class TextSummarizer:
         summary = summarizer(text)
         return summary
 
-    def luhn_summarizer(self, text, n_sentences=3):
+    def luhn_summarizer(self, text, n_sentences=5):
         parser = PlaintextParser.from_string(text, Tokenizer("english"))
         summary = self.summarizer_luhn(parser.document, n_sentences)              
         return " ".join(str(sentence) for sentence in summary)
 
-    def lsa_summarizer(self, text, n_sentences=3):
+    def lsa_summarizer(self, text, n_sentences=5):
         parser = PlaintextParser.from_string(text, Tokenizer("english"))
         summary = self.summarizer_lsa(parser.document, n_sentences)              
         return " ".join(str(sentence) for sentence in summary)
 
 
-    def lex_rank_summarizer(self, text, n_sentences=3):
+    def lex_rank_summarizer(self, text, n_sentences=5):
         parser = PlaintextParser.from_string(text, Tokenizer("english"))
-        summarizer_lex = LexRankSummarizer()                      
         summary = self.summarizer_lexrank(parser.document, n_sentences)              
         return " ".join(str(sentence) for sentence in summary)
 
 
-    def text_rank_summarizer(self, text, n_sentences=3):
+    def text_rank_summarizer(self, text, n_sentences=5):
         parser = PlaintextParser.from_string(text, Tokenizer("english"))
         summary = self.summarizer_textrank(parser.document, n_sentences)
         return " ".join(str(sentence) for sentence in summary)
     
-    def facebook_bart_summarizer(self, text, min_length=30, max_length=90, max_input_length=1024):
+    def facebook_bart_summarizer(self, text, min_length=30, max_length=130, max_input_length=1024):
         input_length = len(text.split())
-        max_length = min(max_length, input_length // 2)
-        min_length = min(min_length, max_length - 1)
+        max_length = input_length if input_length < max_length else max_length        
+        min_length = min(min_length, max_length // 2)
         return self.summarizer_bart(text[:max_input_length], min_length=min_length, max_length=max_length, do_sample=False)[0]["summary_text"]
 
 
